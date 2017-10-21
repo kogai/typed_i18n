f608fca77c6e2f4fb72773ac4b7dcc16205c8a6a
shinchi kogai <kogai0121@gmail.com>
2017-10-21 12:03:35 +0900

    Simplify walk fn

--- src/typed_i18n.ml
+++ src/typed_i18n.ml
@@ -11,49 +11,24 @@ let insert_dot k1 k2 =
 
 (* json -> t list *)
 let walk json =
-  let rec walk_impl key = function
-    | `Assoc xs ->
-      let current = {
-        path = key;
-        value = `Assoc xs;
-      } in
-      let children = List.fold
-          xs
-          ~init:[]
-          ~f:(fun acc (k, v) -> acc @ walk_impl (insert_dot key k) v) in
-      current :: children
-    | `List xs ->
-      let current = {
-        path = key;
-        value = `List xs;
-      } in
+  let rec walk_impl path = Yojson.Basic.(function
+      | `Assoc xs as value ->
+        let current = { path; value; } in
+        let children = List.fold
+            xs
+            ~init:[]
+            ~f:(fun acc (k, v) -> acc @ walk_impl (insert_dot path k) v) in
+        current :: children
+      | `List xs as value ->
+        let current = { path; value; } in
+        let children = List.foldi
+            xs
+            ~init:[]
+            ~f:(fun i acc x -> acc @ walk_impl (insert_dot path "[" ^ string_of_int i ^ "]") x) in
 
-      let children = List.foldi
-          xs
-          ~init:[]
-          ~f:(fun i acc x -> acc @ walk_impl (insert_dot key "[" ^ string_of_int i ^ "]") x) in
-
-      current :: children
-    | `Bool x -> [{
-        path = key;
-        value = `Bool x
-      }]
-    | `Float x -> [{
-        path = key;
-        value = `Float x
-      }]
-    | `Int x -> [{
-        path = key;
-        value = `Int x
-      }]
-    | `String x -> [{
-        path = key;
-        value = `String x
-      }]
-    | `Null -> [{
-        path = key;
-        value = `Null
-      }]
+        current :: children
+      | value -> [{ path; value; }]
+    )
   in
   walk_impl "" json
 
