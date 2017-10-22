6036345a090082bf80ba25d4fdfd78b770cc23b9
shinchi kogai <kogai0121@gmail.com>
2017-10-22 12:28:09 +0900

    Track example

---
+++ example/src/sample.js
@@ -0,0 +1,10 @@
+// @flow
+import type { TFunction } from './locale';
+
+declare var t: TFunction;
+
+const x = t("invalid.string") // ExpectError
+const y = t("children.[1].first_name")
+const zs = t("body_copies")
+
+zs.map(z => (z: number)) // ExpectError
