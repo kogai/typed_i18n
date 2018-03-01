d9caa015ca158aedd7a13e00c4dbfafc4f057333
shinchi kogai <kogai0121@gmail.com>
2018-03-02 08:08:17 +0900

    Consider to use Lwt_log_js

--- fixture/locale.translation.d.ts
+++ fixture/locale.translation.d.ts
@@ -1,35 +1,35 @@
 declare namespace typed_i18n {
   interface TFunction {
-  (_: "title", _?: {}): string; (_: "body_copies", _?: {}): string[];
-  (_: "body_copies.[0]", _?: {}): string;
-  (_: "body_copies.[1]", _?: {}): string;
-  (_: "body_copies.[2]", _?: {}): string;
-  (_: "support_tuple", _?: {}): [ string, number, boolean ];
-  (_: "support_tuple.[0]", _?: {}): string;
-  (_: "support_tuple.[1]", _?: {}): number;
-  (_: "support_tuple.[2]", _?: {}): boolean;
-  (_: "array_of_array", _?: {}): number[][];
-  (_: "array_of_array.[0]", _?: {}): number[];
-  (_: "array_of_array.[0].[0]", _?: {}): number;
-  (_: "array_of_array.[0].[1]", _?: {}): number;
-  (_: "array_of_array.[1]", _?: {}): number[];
-  (_: "array_of_array.[1].[0]", _?: {}): number;
-  (_: "array_of_array.[1].[1]", _?: {}): number;
-  (_: "tuple_of_tuple", _?: {}): [
+  (_: "title", __?: {}): string; (_: "body_copies", __?: {}): string[];
+  (_: "body_copies.[0]", __?: {}): string;
+  (_: "body_copies.[1]", __?: {}): string;
+  (_: "body_copies.[2]", __?: {}): string;
+  (_: "support_tuple", __?: {}): [ string, number, boolean ];
+  (_: "support_tuple.[0]", __?: {}): string;
+  (_: "support_tuple.[1]", __?: {}): number;
+  (_: "support_tuple.[2]", __?: {}): boolean;
+  (_: "array_of_array", __?: {}): number[][];
+  (_: "array_of_array.[0]", __?: {}): number[];
+  (_: "array_of_array.[0].[0]", __?: {}): number;
+  (_: "array_of_array.[0].[1]", __?: {}): number;
+  (_: "array_of_array.[1]", __?: {}): number[];
+  (_: "array_of_array.[1].[0]", __?: {}): number;
+  (_: "array_of_array.[1].[1]", __?: {}): number;
+  (_: "tuple_of_tuple", __?: {}): [
   [ string, boolean, { readonly "foo": string, readonly "bar": null } ],
   [ string, boolean, number ]
 ];
-  (_: "tuple_of_tuple.[0]", _?: {}): [ string, boolean, { readonly "foo": string, readonly "bar": null } ];
-  (_: "tuple_of_tuple.[0].[0]", _?: {}): string;
-  (_: "tuple_of_tuple.[0].[1]", _?: {}): boolean;
-  (_: "tuple_of_tuple.[0].[2]", _?: {}): { readonly "foo": string, readonly "bar": null };
-  (_: "tuple_of_tuple.[0].[2].foo", _?: {}): string;
-  (_: "tuple_of_tuple.[0].[2].bar", _?: {}): null;
-  (_: "tuple_of_tuple.[1]", _?: {}): [ string, boolean, number ];
-  (_: "tuple_of_tuple.[1].[0]", _?: {}): string;
-  (_: "tuple_of_tuple.[1].[1]", _?: {}): boolean;
-  (_: "tuple_of_tuple.[1].[2]", _?: {}): number;
-  (_: "child", _?: {}): {
+  (_: "tuple_of_tuple.[0]", __?: {}): [ string, boolean, { readonly "foo": string, readonly "bar": null } ];
+  (_: "tuple_of_tuple.[0].[0]", __?: {}): string;
+  (_: "tuple_of_tuple.[0].[1]", __?: {}): boolean;
+  (_: "tuple_of_tuple.[0].[2]", __?: {}): { readonly "foo": string, readonly "bar": null };
+  (_: "tuple_of_tuple.[0].[2].foo", __?: {}): string;
+  (_: "tuple_of_tuple.[0].[2].bar", __?: {}): null;
+  (_: "tuple_of_tuple.[1]", __?: {}): [ string, boolean, number ];
+  (_: "tuple_of_tuple.[1].[0]", __?: {}): string;
+  (_: "tuple_of_tuple.[1].[1]", __?: {}): boolean;
+  (_: "tuple_of_tuple.[1].[2]", __?: {}): number;
+  (_: "child", __?: {}): {
   readonly "title_of_child": string,
   readonly "variable_types": {
     readonly "key_of_int": number,
@@ -38,29 +38,29 @@ declare namespace typed_i18n {
     readonly "key_of_null": null
   }
 };
-  (_: "child.title_of_child", _?: {}): string;
-  (_: "child.variable_types", _?: {}): {
+  (_: "child.title_of_child", __?: {}): string;
+  (_: "child.variable_types", __?: {}): {
   readonly "key_of_int": number,
   readonly "key_of_float": number,
   readonly "key_of_boolean": boolean,
   readonly "key_of_null": null
 };
-  (_: "child.variable_types.key_of_int", _?: {}): number;
-  (_: "child.variable_types.key_of_float", _?: {}): number;
-  (_: "child.variable_types.key_of_boolean", _?: {}): boolean;
-  (_: "child.variable_types.key_of_null", _?: {}): null;
-  (_: "children", _?: {}): { readonly "first_name": string, readonly "familly_name": string }[];
-  (_: "children.[0]", _?: {}): { readonly "first_name": string, readonly "familly_name": string };
-  (_: "children.[0].first_name", _?: {}): string;
-  (_: "children.[0].familly_name", _?: {}): string;
-  (_: "children.[1]", _?: {}): { readonly "first_name": string, readonly "familly_name": string };
-  (_: "children.[1].first_name", _?: {}): string;
-  (_: "children.[1].familly_name", _?: {}): string;
-  (_: "array_of_empty_object", _?: {}): {}[];
-  (_: "array_of_empty_object.[0]", _?: {}): {};
-  (_: "allow-comupted-properties", _?: {}): { readonly "0": string, readonly "foo-bar": boolean };
-  (_: "allow-comupted-properties.0", _?: {}): string;
-  (_: "allow-comupted-properties.foo-bar", _?: {}): boolean
+  (_: "child.variable_types.key_of_int", __?: {}): number;
+  (_: "child.variable_types.key_of_float", __?: {}): number;
+  (_: "child.variable_types.key_of_boolean", __?: {}): boolean;
+  (_: "child.variable_types.key_of_null", __?: {}): null;
+  (_: "children", __?: {}): { readonly "first_name": string, readonly "familly_name": string }[];
+  (_: "children.[0]", __?: {}): { readonly "first_name": string, readonly "familly_name": string };
+  (_: "children.[0].first_name", __?: {}): string;
+  (_: "children.[0].familly_name", __?: {}): string;
+  (_: "children.[1]", __?: {}): { readonly "first_name": string, readonly "familly_name": string };
+  (_: "children.[1].first_name", __?: {}): string;
+  (_: "children.[1].familly_name", __?: {}): string;
+  (_: "array_of_empty_object", __?: {}): {}[];
+  (_: "array_of_empty_object.[0]", __?: {}): {};
+  (_: "allow-comupted-properties", __?: {}): { readonly "0": string, readonly "foo-bar": boolean };
+  (_: "allow-comupted-properties.0", __?: {}): string;
+  (_: "allow-comupted-properties.foo-bar", __?: {}): boolean
 }
 }
 export = typed_i18n;
